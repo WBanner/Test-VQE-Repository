2a84ba9a21c59102ed4d165562928d37aa4d7c9a
WBanner <64441813+WBanner@users.noreply.github.com>
2020-04-28 14:00:45 -0400

    fixed operator count bug
    now allows for counting of additional adapt

--- adapt_data_processing.py
+++ adapt_data_processing.py
@@ -17,6 +17,11 @@ adapt_roto_2_param_df = pd.read_csv('adapt_roto_2_param_df.csv')
 adapt_roto_2_op_df = pd.read_csv('adapt_roto_2_op_df.csv')
 adapt_roto_2_E_df = pd.read_csv('adapt_roto_2_E_df.csv')
 
+adapt_roto_3_data_df = pd.read_csv('adapt_roto_3_data_df.csv')
+adapt_roto_3_param_df = pd.read_csv('adapt_roto_3_param_df.csv')
+adapt_roto_3_op_df = pd.read_csv('adapt_roto_3_op_df.csv')
+adapt_roto_3_E_df = pd.read_csv('adapt_roto_3_E_df.csv')
+
 adapt_data_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
 adapt_param_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
 adapt_op_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
@@ -29,62 +34,69 @@ adapt_roto_2_data_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
 adapt_roto_2_param_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
 adapt_roto_2_op_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
 adapt_roto_2_E_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
+adapt_roto_3_data_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
+adapt_roto_3_param_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
+adapt_roto_3_op_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
+adapt_roto_3_E_df.drop(axis = 1,columns = ['Unnamed: 0'], inplace = True)
 
 shape = adapt_E_df.shape
 numrows = shape[0]
 
-adapt_roto_E_metadata = {'adapt mean E': [], 'R1 mean E': [], 'R2 mean E': [], 'adapt std E': [], 'R1 std E': [], 'R2 std E': []}
-adapt_roto_op_metadata ={'num ops same adapt R1': [], 'num ops same adapt R2': [], 'num ops same R1 R2':[]}
-adapt_roto_param_metadata = {'mean param dif adapt R1': [], 'mean param dif adapt R2': [], 'mean param dif R1 R2': [], 'std param dif adapt R1': [], 'std param dif adapt R2': [], 'std param dif R1 R2': []}
+adapt_roto_E_metadata = {'adapt mean E': [], 'R1 mean E': [], 'R2 mean E': [], 'R3 mean E': [], 'adapt std E': [], 'R1 std E': [], 'R2 std E': [], 'R3 std E': []}
+adapt_roto_op_metadata ={'num ops same adapt R3': [], 'num ops same R1 R2': [], 'num ops same R2 R3':[], }
+adapt_roto_param_metadata = {'mean param dif adapt R3': [], 'mean param dif R1 R2': [], 'mean param dif R2 R3': [], 'std param dif adapt R3': [], 'std param dif R1 R2': [], 'std param dif R2 R3': []}
 adapt_roto_metadata = {'eval time': [], 'num evals': [], 'final energy': [], 'final energy std': [], 'same struct count': [], 'mean param dif': []}
 
 #update energy dictionary
 adapt_roto_E_metadata['adapt mean E'] = list(adapt_E_df.mean(axis=1))
 adapt_roto_E_metadata['R1 mean E'] = list(adapt_roto_1_E_df.mean(axis=1))
 adapt_roto_E_metadata['R2 mean E'] = list(adapt_roto_2_E_df.mean(axis=1))
+adapt_roto_E_metadata['R3 mean E'] = list(adapt_roto_3_E_df.mean(axis=1))
 adapt_roto_E_metadata['adapt std E'] = list(adapt_E_df.std(axis=1))
 adapt_roto_E_metadata['R1 std E'] = list(adapt_roto_1_E_df.std(axis=1))
 adapt_roto_E_metadata['R2 std E'] = list(adapt_roto_2_E_df.std(axis=1))
+adapt_roto_E_metadata['R3 std E'] = list(adapt_roto_3_E_df.std(axis=1))
 
 #update op dictionary
 
 #count number of ops that are same starting from end
-ttable_a = adapt_op_df.eq(adapt_roto_1_op_df)
-ttable_1 = adapt_op_df.eq(adapt_roto_2_op_df)
-ttable_2 = adapt_roto_1_op_df.eq(adapt_roto_2_op_df)
+ttable_a = adapt_op_df.eq(adapt_roto_3_op_df)
+ttable_1 = adapt_roto_1_op_df.eq(adapt_roto_2_op_df)
+ttable_2 = adapt_roto_2_op_df.eq(adapt_roto_3_op_df)
 
-adapt_R1_param_dif_df = adapt_param_df.sub(adapt_roto_1_param_df)
-adapt_R2_param_dif_df = adapt_param_df.sub(adapt_roto_2_param_df)
+adapt_R3_param_dif_df = adapt_param_df.sub(adapt_roto_3_param_df)
 R1_R2_param_dif_df = adapt_roto_1_param_df.sub(adapt_roto_2_param_df)
+R2_R3_param_dif_df = adapt_roto_2_param_df.sub(adapt_roto_3_param_df)
 
 for i in range(0,numrows): 
 	if ttable_a.iloc[i].value_counts().shape[0] == 2:
-		adapt_roto_op_metadata['num ops same adapt R1'].append(ttable_a.iloc[i].value_counts().iloc[0])
+		adapt_roto_op_metadata['num ops same adapt R3'].append(ttable_a.iloc[i].value_counts().iloc[0])
+		print(ttable_a.iloc[i].value_counts().iloc[0])
 	else:
-		adapt_roto_op_metadata['num ops same adapt R1'].append(0)
+		adapt_roto_op_metadata['num ops same adapt R3'].append(0)
 	if ttable_1.iloc[i].value_counts().shape[0] == 2:
-		adapt_roto_op_metadata['num ops same adapt R2'].append(ttable_1.iloc[i].value_counts().iloc[0])
-	else:
-		adapt_roto_op_metadata['num ops same adapt R2'].append(0)
-	if ttable_2.iloc[i].value_counts().shape[0] == 2:
-		adapt_roto_op_metadata['num ops same R1 R2'].append(ttable_2.iloc[i].value_counts().iloc[0])
+		adapt_roto_op_metadata['num ops same R1 R2'].append(ttable_1.iloc[i].value_counts().iloc[0])
 	else:
 		adapt_roto_op_metadata['num ops same R1 R2'].append(0)
+	if ttable_2.iloc[i].value_counts().shape[0] == 2:
+		adapt_roto_op_metadata['num ops same R2 R3'].append(ttable_2.iloc[i].value_counts().iloc[0])
+	else:
+		adapt_roto_op_metadata['num ops same R2 R3'].append(0)
 	if len(list(ttable_a.iloc[i][ttable_a.iloc[i]==False].index)):
 		ttable_a = ttable_a.drop(None, axis=1, index=None, columns = list(ttable_a.iloc[i][ttable_a.iloc[i] == False].index))
-		adapt_R1_param_dif_df = adapt_R1_param_dif_df.drop(None, axis=1, index=None, columns = list(ttable_a.iloc[i][ttable_a.iloc[i] == False].index))
+		adapt_R3_param_dif_df = adapt_R3_param_dif_df.drop(None, axis=1, index=None, columns = list(ttable_a.iloc[i][ttable_a.iloc[i] == False].index))
 	if len(list(ttable_1.iloc[i][ttable_1.iloc[i] == False].index)):
 		ttable_1 = ttable_1.drop(None, axis=1, index=None, columns = list(ttable_1.iloc[i][ttable_1.iloc[i] == False].index))
-		adapt_R2_param_dif_df = adapt_R2_param_dif_df.drop(None, axis=1, index=None, columns = list(ttable_1.iloc[i][ttable_1.iloc[i] == False].index))
+		R1_R2_param_dif_df = R1_R2_param_dif_df.drop(None, axis=1, index=None, columns = list(ttable_1.iloc[i][ttable_1.iloc[i] == False].index))
 	if len(list(ttable_2.iloc[i][ttable_2.iloc[i] == False].index)):
 		ttable_2 = ttable_2.drop(None, axis=1, index=None, columns = list(ttable_2.iloc[i][ttable_2.iloc[i] == False].index))
-		R1_R2_param_dif_df = R1_R2_param_dif_df.drop(None, axis=1, index=None, columns = list(ttable_2.iloc[i][ttable_2.iloc[i] == False].index))
-	adapt_roto_param_metadata['mean param dif adapt R1'].append(adapt_R1_param_dif_df.iloc[i].mean())
-	adapt_roto_param_metadata['mean param dif adapt R2'].append(adapt_R2_param_dif_df.iloc[i].mean())
+		R2_R3_param_dif_df = R2_R3_param_dif_df.drop(None, axis=1, index=None, columns = list(ttable_2.iloc[i][ttable_2.iloc[i] == False].index))
+	adapt_roto_param_metadata['mean param dif adapt R3'].append(adapt_R3_param_dif_df.iloc[i].mean())
 	adapt_roto_param_metadata['mean param dif R1 R2'].append(R1_R2_param_dif_df.iloc[i].mean())
-	adapt_roto_param_metadata['std param dif adapt R1'].append(adapt_R1_param_dif_df.iloc[i].std())
-	adapt_roto_param_metadata['std param dif adapt R2'].append(adapt_R2_param_dif_df.iloc[i].std())
+	adapt_roto_param_metadata['mean param dif R2 R3'].append(R2_R3_param_dif_df.iloc[i].mean())
+	adapt_roto_param_metadata['std param dif adapt R3'].append(adapt_R3_param_dif_df.iloc[i].std())
 	adapt_roto_param_metadata['std param dif R1 R2'].append(R1_R2_param_dif_df.iloc[i].std())
+	adapt_roto_param_metadata['std param dif R2 R3'].append(R2_R3_param_dif_df.iloc[i].std())
 
 #update param dictionary
 
@@ -93,31 +105,37 @@ adapt_roto_2_data_dict = {'hamiltonian': [], 'eval time': [], 'num evals': [], '
 adapt_roto_metadata['eval time'].append(adapt_data_df['eval time'].mean())
 adapt_roto_metadata['eval time'].append(adapt_roto_1_data_df['eval time'].mean())
 adapt_roto_metadata['eval time'].append(adapt_roto_2_data_df['eval time'].mean())
+adapt_roto_metadata['eval time'].append(adapt_roto_3_data_df['eval time'].mean())
 
 adapt_roto_metadata['num evals'].append(adapt_data_df['num evals'].mean())
 adapt_roto_metadata['num evals'].append(adapt_roto_1_data_df['num evals'].mean())
 adapt_roto_metadata['num evals'].append(adapt_roto_2_data_df['num evals'].mean())
+adapt_roto_metadata['num evals'].append(adapt_roto_3_data_df['num evals'].mean())
 
 adapt_roto_metadata['final energy'].append(adapt_data_df['final energy'].mean())
 adapt_roto_metadata['final energy'].append(adapt_roto_1_data_df['final energy'].mean())
 adapt_roto_metadata['final energy'].append(adapt_roto_2_data_df['final energy'].mean())
+adapt_roto_metadata['final energy'].append(adapt_roto_3_data_df['final energy'].mean())
 
 adapt_roto_metadata['final energy std'].append(adapt_data_df['final energy'].std())
 adapt_roto_metadata['final energy std'].append(adapt_roto_1_data_df['final energy'].std())
 adapt_roto_metadata['final energy std'].append(adapt_roto_2_data_df['final energy'].std())
+adapt_roto_metadata['final energy std'].append(adapt_roto_3_data_df['final energy'].std())
 
-adapt_roto_metadata['same struct count'].append(adapt_roto_op_metadata['num ops same adapt R1'][-1])
-adapt_roto_metadata['same struct count'].append(adapt_roto_op_metadata['num ops same adapt R2'][-1])
+adapt_roto_metadata['same struct count'].append(adapt_roto_op_metadata['num ops same adapt R3'][-1])
 adapt_roto_metadata['same struct count'].append(adapt_roto_op_metadata['num ops same R1 R2'][-1])
+adapt_roto_metadata['same struct count'].append(adapt_roto_op_metadata['num ops same R2 R3'][-1])
+adapt_roto_metadata['same struct count'].append(0)
 
 
 
 adapt_roto_param_metadata_df = pd.DataFrame(adapt_roto_param_metadata)
 
 #something up with mean here
-adapt_roto_metadata['mean param dif'].append(adapt_roto_param_metadata_df['mean param dif adapt R1'].mean())
-adapt_roto_metadata['mean param dif'].append(adapt_roto_param_metadata_df['mean param dif adapt R2'].mean())
+adapt_roto_metadata['mean param dif'].append(adapt_roto_param_metadata_df['mean param dif adapt R3'].mean())
 adapt_roto_metadata['mean param dif'].append(adapt_roto_param_metadata_df['mean param dif R1 R2'].mean())
+adapt_roto_metadata['mean param dif'].append(adapt_roto_param_metadata_df['mean param dif R2 R3'].mean())
+adapt_roto_metadata['mean param dif'].append(0)
 
 
 adapt_roto_metadata_df = pd.DataFrame(adapt_roto_metadata)
